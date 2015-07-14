01f06652267ed3c78157bccf1543bf0277cd0130
pires.info@gmail.com <pires.info@gmail.com>
2015-07-14 14:15:52 -0300

    init

---
+++ src/main/java/SampleController.java
@@ -0,0 +1,24 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+    	String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+        System.setProperty("server.port", webPort);
+        SpringApplication.run(SampleController.class, args);
+    }
+}
